99ccd17ca9feaf22241b8cd07b58980dea0f4011
G0Yang <39620810+G0Yang@users.noreply.github.com>
2018-08-10 19:39:02 +0900

    2018.08.10 패치

--- synchronize/synchronize.py
+++ synchronize/synchronize.py
@@ -1,47 +1,47 @@
-import time, sys, os, json, pathlib, socket, threading
+# 원장이 해시값으로 잘 연결되어 있는지 확인
+# -> 아니라면 잘린 부분 이후를 삭제
+# -> 나중에 타 노드에게 요청할 수 있음.
+
+# 타 노드의 원장과 비교하여 다른 점이 있는지 확인
+# -> 최소 3개 이상
+# -> 다르다면 
+
+import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-from chaincode.chainToJson import *
-from worldstate.kvstore import kvstore
-
-# 기존 체인안에 있는 블록들이 헤시값으로 잘 연결되어 있는지 확인함.
-# 각 체인의 마지막 블록과 W에 저장되어 있는 블록값이 일치하는 지 확인.
-# 같지 않다면 기존 블록을 처리하는 알고리즘을 구현.
+from flagdb.kvstore import kvstore
 
 class synchronize:
     def __init__(self, *args, **kwargs):
+        self.flag = True
+        self.Chain = None
         return
 
-    def isPriviousHash(self, ctj):
-        print(type(ctj.data))
-        print(ctj.data.toDict())
+    def get_FlagDB(self, CHID):
+
+        return False
+
+    def isCurrectHashInChain(self,Chain):
+        if type(Chain) != "<class 'ledger.chain.chain'>":
+            return False
+        self.Chain = Chain
+        for i in renge(0,len(Chain.chains)-1):
+            if Chain.Chians[i].BH.currentBlockHash is not Chain.Chians[i+1].BH.previousBlockHash:
+                self.flag = False
+        return self.flag
+
+    def compareToFlagDB(self):
+        if self.Chain is None:
+            return False
+        try :
+            local_LastBlock = Chain.chains[-1]
+            FalgDB_lastBlock = get_FlagDB(CHID = self.Chain.CHID)
+
+            # 비교 연산...
 
 
-
-        # 블록이 이어디는 것을 Hash를 이용하여 탐색
-        # 재귀함수 형식으로 재작하여 최종 마지막 블록의 Hash값을 반환하게 제작
-
-    #def callWorldState(self, block):
-        #중간에 잘린 블록을 얻기위해서 W에 최신 정보와 잘려진 마지막 블록을 대조하여 잘린 블록인지, 이어진 블록인지 판별
-
-
-    def checkLastBlock(self):
-        return
-
-
-
-
-if __name__ == "__main__":
-    cj1 = chainToJson(filename = "8srkziel366214.json")
-    print(cj1)
-
-    sync = synchronize()
-    sync.isPriviousHash(cj1)
-    '''
-    for i in range(1, len(ctj.data)):
-        if ctj.data[str(i)]['BH_currentBlockHash'] == ctj.data[str(i+1)]['BH_previousBlockHash']:
-            print(i, "matched!!")
-            continue
+        except:
+            return False
         else:
-            print("not match!!")
-    '''
+            return True
+        return False
